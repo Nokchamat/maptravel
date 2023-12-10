ad6dc61a882ce364cfe8b762db14fa3eaa15bbc7
NokchaMat <rkdtjd976@gmail.com>
2023-12-10 16:45:31 +0900

    ### Bookmark - 북마크 사진 터치 시 해당 게시물로 이동

--- lib/bookmark/w_bookmark.dart
+++ lib/bookmark/w_bookmark.dart
@@ -3,24 +3,36 @@ import 'package:http/http.dart' as http;
 import 'package:maptravel/api/api_bookmark.dart';
 
 import '../dto/vo_bookmark.dart';
+import '../plane_detail/s_plane_detail.dart';
 import '../sign/f_login.dart';
 
 class BookmarkWidget extends StatelessWidget {
   final Bookmark bookmark;
   final VoidCallback onRemove;
 
-  const BookmarkWidget({required this.bookmark, super.key, required this.onRemove});
+  const BookmarkWidget(
+      {required this.bookmark, super.key, required this.onRemove});
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Image.network(
-            bookmark.thumbnailUrl,
-            fit: BoxFit.cover,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PlaneDetailScreen(
+                      planeId: bookmark.planeId,
+                    )));
+          },
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Image.network(
+              bookmark.thumbnailUrl,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         Positioned(
@@ -41,29 +53,33 @@ class BookmarkWidget extends StatelessWidget {
             padding: const EdgeInsets.all(5.0),
             child: IconButton(
               onPressed: () async {
-                http.Response response = await removeBookmark(bookmark.planeId)
-                    .catchError((onError) => {
-                          print(onError),
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const LoginFragment())),
-                        });
+                http.Response response =
+                    await removeBookmark(bookmark.planeId).catchError(
+                  (onError) => {
+                    print(onError),
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginFragment())),
+                  },
+                );
 
                 if (response.statusCode == 200) {
                   onRemove();
                 }
               },
-              icon: const Icon(Icons.bookmark,
-                  size: 30,
-                  color: Colors.white,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 5.0,
-                      color: Colors.green,
-                      offset: Offset(1.0, 1.0),
-                    )
-                  ]),
+              icon: const Icon(
+                Icons.bookmark,
+                size: 30,
+                color: Colors.white,
+                shadows: [
+                  Shadow(
+                    blurRadius: 5.0,
+                    color: Colors.green,
+                    offset: Offset(1.0, 1.0),
+                  )
+                ],
+              ),
             ),
           ),
         ),
