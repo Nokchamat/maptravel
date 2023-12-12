76776e364130961d8677428030786b6a20541f9b
NokchaMat <rkdtjd976@gmail.com>
2023-12-12 15:25:28 +0900

    ### LoginFragment Pop - 로그인 프래그먼트로 진입 후 뒤로가기를 눌렀을 때 홈으로 갈 수 있도록 수정

--- lib/sign/f_login.dart
+++ lib/sign/f_login.dart
@@ -28,100 +28,96 @@ class _LoginFragmentState extends State<LoginFragment> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('로그인'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1.0,
-                ),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: TextField(
-                  controller: _email,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Email',
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1.0,
-                ),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: TextField(
-                  controller: _password,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Password',
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () async {
-                final response = await http.post(
-                  Uri.parse('$baseUrl/v1/signin'),
-                  headers: <String, String>{
-                    'Content-Type': 'application/json',
-                  },
-                  body: jsonEncode({
-                    "email": _email.text,
-                    "password": _password.text,
-                  }),
-                );
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MyApp()),
+        );
 
-                if (response.statusCode == 200) {
-                  print('로그인 완료');
-                  login(Token(
-                    response.headers['access_token']!,
-                    response.headers['refresh_token']!,
-                  ));
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MyApp()));
-                } else {
-                  // 로그인 실패 시 알럿 창 띄우기
-                  print('로그인 실패');
-                  showAlertDialog(context,
-                      json.decode(utf8.decode(response.bodyBytes))['message']);
-                }
-              },
-              child: Text('로그인 하기'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await signIn(_email.text, _password.text).catchError(
-                  (onError) {
-                    print(onError);
-                  },
-                ).then(
-                  (token) => {
-                    login(token),
-                  },
-                );
-              },
-              child: Text('확인'),
-            ),
-          ],
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('로그인'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    controller: _email,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Email',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    controller: _password,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Password',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () async {
+                  final response = await http.post(
+                    Uri.parse('$baseUrl/v1/signin'),
+                    headers: <String, String>{
+                      'Content-Type': 'application/json',
+                    },
+                    body: jsonEncode({
+                      "email": _email.text,
+                      "password": _password.text,
+                    }),
+                  );
+
+                  if (response.statusCode == 200) {
+                    print('로그인 완료');
+                    login(Token(
+                      response.headers['access_token']!,
+                      response.headers['refresh_token']!,
+                    ));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MyApp()));
+                  } else {
+                    // 로그인 실패 시 알럿 창 띄우기
+                    print('로그인 실패');
+                    showAlertDialog(context,
+                        json.decode(utf8.decode(response.bodyBytes))['message']);
+                  }
+                },
+                child: Text('로그인 하기'),
+              ),
+            ],
+          ),
         ),
       ),
     );
